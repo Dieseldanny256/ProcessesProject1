da25b44495b12770162ef38f0a745eda3e3938c3
Daniel Bakos <danieltbakos@gmail.com>
2025-01-25 15:25:59 -0500

    Minor Styling
    Getting started with styling the login page.

--- js/infogoblins.js
+++ js/infogoblins.js
@@ -69,10 +69,10 @@ function doSignUp()
     //Get values from field
     let newFirstName = document.getElementById("firstName").value;
     let newLastName = document.getElementById("lastName").value;
-    let username = document.getElementById("loginName").value;
-    let password = document.getElementById("loginPassword").value;
+    let username = document.getElementById("registerName").value;
+    let password = document.getElementById("registerPassword").value;
 
-    document.getElementById("loginResult").innerHTML = ""; //Set the result message field to nothing
+    document.getElementById("registerResult").innerHTML = ""; //Set the result message field to nothing
 
     let packet = {"firstName":newFirstName, "lastName":newLastName, "login":username, "password":password}; //Generate a packet to send
     let jsonPayload = JSON.stringify(packet); //Generates the packet
@@ -91,7 +91,7 @@ function doSignUp()
             {
                 let jsonObject = JSON.parse(xhr.responseText); //Converts the packet to an object
                 if (jsonObject.error != "") {
-                    document.getElementById("loginResult").innerHTML = jsonObject.error;
+                    document.getElementById("registerResult").innerHTML = jsonObject.error;
 					return;
                 }
 				userId = jsonObject.results[0].id; //sets the userId to this newly obtained userId 
@@ -109,7 +109,7 @@ function doSignUp()
     }
     catch(err)
     {
-        document.getElementById("loginResult").innerHTML = err.message; //Displays the error
+        document.getElementById("registerResult").innerHTML = err.message; //Displays the error
     }
 }
 
@@ -173,6 +173,47 @@ function readCookie()
 	{
 		document.getElementById("name").innerHTML = firstName + " " + lastName; //Login to the page
 	}
+}
+
+function loadContactData()
+{
+    let packet = {"search":"", "lastName":newLastName}; //Generate a packet to send (search for empty string)
+    let jsonPayload = JSON.stringify(packet); //Generates the packet
+
+    let url = urlBase + '/SignUp.' + extension //generates the signUp url
+
+    let xhr = new XMLHttpRequest(); //Generates a new HttpRequest object
+    xhr.open("POST", url, true); //Initializes the xhr module for requests
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        //Called when the packet is recieved
+        xhr.onreadystatechange = function()
+        {
+            if (this.readyState == 4 && this.status == 200)
+            {
+                let jsonObject = JSON.parse(xhr.responseText); //Converts the packet to an object
+                if (jsonObject.error != "") {
+                    document.getElementById("registerResult").innerHTML = jsonObject.error;
+					return;
+                }
+				userId = jsonObject.results[0].id; //sets the userId to this newly obtained userId 
+                
+                //Registration was valid
+				firstName = newFirstName;
+				lastName = newLastName;
+
+				saveCookie(); //Saves the cookie (whatever that means)
+                
+				window.location.href = "contacts.html"; //Redirects to the main page
+            }
+        };
+        xhr.send(jsonPayload); //Send the packet
+    }
+    catch(err)
+    {
+        document.getElementById("registerResult").innerHTML = err.message; //Displays the error
+    }
 }
 
 function doLogout()
